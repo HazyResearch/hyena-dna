fd399a8a6651421fedb6a72c311b6964b23da6bb
d-laub <dlaub@ucsd.edu>
2024-02-01 14:21:17 -0800

    feat: working seqlen warmup

---
+++ src/callbacks/multifasta_seqlen_warmup.py
@@ -0,0 +1,68 @@
+from typing import cast
+
+import numpy as np
+import polars as pl
+import pytorch_lightning as lit
+
+from src.dataloaders.multifasta import MultiFasta
+
+
+class SeqLenWarmup(lit.Callback):
+    def __init__(self, max_length: int, tokens_per_step: int, init_length: int = 2**10):
+        if max_length <= init_length:
+            raise ValueError("max_length must be greater than init_length")
+
+        self.tokens_per_step = tokens_per_step
+
+        lengths = 2 ** np.arange(
+            np.log2(init_length), np.ceil(np.log2(max_length)) + 1, dtype=int
+        )
+        lengths[-1] = max_length
+        self.lengths = lengths.astype(int)
+        self.step = 0
+        self.og_beds = None
+        self.max_tokens_per_batch = None
+
+    def on_train_epoch_start(
+        self, trainer: lit.Trainer, pl_module: lit.LightningModule
+    ) -> None:
+        dm = cast(MultiFasta, pl_module.dataset)  # type: ignore[reportGeneralTypeIssues]
+        if self.og_beds is None and self.max_tokens_per_batch is None:
+            self.og_beds = dm.beds
+            self.max_tokens_per_batch = int(dm.batch_size * self.lengths[-1])
+
+        current_length = int(self.lengths[self.step])
+        dm.batch_size = self.max_tokens_per_batch // current_length
+        pl_module.log("seqlen_warmup_step", self.step)
+        pl_module.log("seqlen_warmup_length", current_length)
+        pl_module.log("seqlen_warmup_batch_size", dm.batch_size)
+
+        if self.step >= len(self.lengths) - 1:
+            pl_module.log("seqlen_warmup_finished", True)
+            dm.beds = self.og_beds
+        else:
+            pl_module.log("seqlen_warmup_finished", False)
+            dm.beds = {
+                k: self._with_length(bed, current_length, self.tokens_per_step, stage=k)
+                for k, bed in self.og_beds.items()
+            }
+        self.step += 1
+
+        dm.setup()
+        trainer.reset_train_dataloader(pl_module)
+        trainer.reset_val_dataloader(pl_module)
+        trainer.reset_test_dataloader(pl_module)
+
+    @staticmethod
+    def _with_length(bed: pl.DataFrame, length: int, n_tokens: int, stage: str):
+        midpt = (pl.col("chromEnd") + pl.col("chromStart")) / 2
+        bed = bed.filter(
+            pl.col("chromEnd") - pl.col("chromStart") >= length
+        ).with_columns(
+            chromEnd=(midpt + length / 2).round(),
+            chromStart=(midpt - length / 2).round(),
+        )
+        if stage == "train":
+            return bed.sample(np.ceil(n_tokens / length), with_replacement=True)
+        else:
+            return bed
--- src/dataloaders/multifasta.py
+++ src/dataloaders/multifasta.py
@@ -1,9 +1,8 @@
-from typing import Dict, Optional, Tuple, Union, cast
+from typing import Dict, Optional, Tuple, Union
 
 import genvarloader as gvl
 import numpy as np
 import polars as pl
-import pytorch_lightning as lit
 from attrs import define
 from numpy.typing import NDArray
 from torch.utils.data import DataLoader, Dataset
@@ -89,23 +88,23 @@ class MultiFasta(SequenceDataset):
             fastas = pl.read_csv(file_table, separator="\t")["fasta"]
         else:
             fastas = file_table["fasta"]
-        
-        fastas = fastas.str.replace(r'/iblm/netapp', r'/home/jovyan', literal=True)
-        
+
+        fastas = fastas.str.replace(r"/iblm/netapp", r"/home/jovyan", literal=True)
+
         if isinstance(bed, str):
             bed = pl.read_ipc(bed)
 
         if limit_fastas is not None:
             fastas = fastas.head(limit_fastas)
-        
+
         species = fastas.str.split("/").list.get(-1).str.split(".").list.get(0)
-        
+
         if limit_fastas is not None:
-            bed = bed.filter(pl.col('species').is_in(species))
-            
+            bed = bed.filter(pl.col("species").is_in(species))
+
         self.beds = bed.partition_by("split", as_dict=True, include_key=False)
         # keep this around in case seqlen warmup used
-        self.full_train = self.beds['train']
+        self.full_train = self.beds["train"]
 
         self.fastas = {
             s: gvl.Fasta(NAME, f, "N", "dna")
@@ -113,9 +112,10 @@ class MultiFasta(SequenceDataset):
                 zip(species, fastas), desc="Init FASTAs", total=len(species)
             )
         }
+        # self.train_batch_size = batch_size
+        self.batch_size = batch_size
         self.dl_kwargs = {
             "pin_memory": True,
-            "batch_size": batch_size,
             "num_workers": num_workers,
         }
 
@@ -125,52 +125,10 @@ class MultiFasta(SequenceDataset):
         self.test_ds = TorchMultiFasta(self.fastas, self.beds["test"])
 
     def train_dataloader(self, **kwargs):
-        return DataLoader(self.train_ds, shuffle=True, **self.dl_kwargs)
+        return DataLoader(self.train_ds, shuffle=True, batch_size=self.batch_size, **self.dl_kwargs)
 
     def val_dataloader(self, **kwargs):
-        return DataLoader(self.val_ds, **self.dl_kwargs)
+        return DataLoader(self.val_ds, self.batch_size, **self.dl_kwargs)
 
     def test_dataloader(self, **kwargs):
-        return DataLoader(self.test_ds, **self.dl_kwargs)
-    
-
-class SeqLenWarmup(lit.Callback):
-    def __init__(self, max_length: int, tokens_per_step: int, init_length: int = 1024):
-        if max_length <= init_length:
-            raise ValueError("max_length must be greater than init_length")
-        
-        self.tokens_per_step = tokens_per_step
-        
-        lengths = 2**np.arange(np.log2(init_length), np.ceil(np.log2(max_length)))
-        lengths[-1] = max_length
-        self.lengths = lengths
-        self.len_idx = 0
-        
-    def on_fit_start(self, trainer: lit.Trainer, pl_module: lit.LightningModule) -> None:
-        dm = cast(MultiFasta, trainer.datamodule)  # type: ignore[reportGeneralTypeIssues]
-        self.full_train = dm.beds['train']
-        
-        dm.beds['train'] = self._with_length(self.full_train, self.lengths[self.len_idx], self.tokens_per_step)
-    
-    def on_train_epoch_end(self, trainer: lit.Trainer, pl_module: lit.LightningModule) -> None:
-        dm = cast(MultiFasta, trainer.datamodule)  # type: ignore[reportGeneralTypeIssues]
-        self.len_idx += 1
-        
-        if self.len_idx == len(self.lengths):
-            dm.beds['train'] = self.full_train
-        else:
-            dm.beds['train'] = self._with_length(self.full_train, self.lengths[self.len_idx], self.tokens_per_step)
-    
-    @staticmethod
-    def _with_length(bed: pl.DataFrame, length: int, n_tokens: int):
-        midpt = (pl.col('chromEnd') + pl.col('chromStart')) / 2
-        
-        return (
-            bed
-            .filter(pl.col('chromEnd') - pl.col('chromStart') >= length)
-            .with_columns(
-                chromEnd=(midpt + length / 2).round(),
-                chromStart=(midpt - length / 2).round()
-            )
-            .sample(np.ceil(n_tokens / length), with_replacement=True)
-        )
+        return DataLoader(self.test_ds, self.batch_size, **self.dl_kwargs)
