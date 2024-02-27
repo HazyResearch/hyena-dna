3e776e820fa2491326a1334043ed5dcd10cb5eb4
Arda Pekis <arda.pekis@simbiosys.com>
2024-02-27 05:05:48 +0000

    quick fix

--- evals/all_checkpoints.sh
+++ evals/all_checkpoints.sh
@@ -8,10 +8,10 @@ for d in /home/ubuntu/HyenaDNA_collab/hyena-dna-repo/mamba_checkpoints/artifacts
 		b=8
 		if [[ "$d" == *"model-l968ycve"* ]]; then
 			g="Cactus 1.4M 64Kibp"
-			m="dataset.batch_size=8"
+			m=""
 		else
 			g="HG38 1.4M 64Kibp"
-			m="dataset.batch_size=8"
+			m=""
 		fi
 	fi
 	#python -m train wandb.job_type=eval experiment=hg38/gvl_hg38_mamba trainer.limit_train_batches=1 optimizer.lr=1e-10 trainer.max_epochs=1 "wandb.group=$g" trainer.devices=8 "train.pretrained_model_path=${d}model.ckpt" ++dataset.fasta=/home/ubuntu/HyenaDNA_collab/hyena-dna-repo/data/hg38/hg38.ml.fa ++dataset.bed=/home/ubuntu/HyenaDNA_collab/hyena-dna-repo/data/hg38/human-sequences.bed $m "dataset.batch_size=$((b/4))"
